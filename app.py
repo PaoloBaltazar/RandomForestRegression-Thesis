84b64a3f6b5835c1ca3049eb58d75a205c7556eb
PaoloBaltazar <gpaolobaltazar@gmail.com>
2024-08-04 01:38:57 +0800

    create static folder and update app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import joblib
 import numpy as np
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 CORS(app)  # Enable CORS for all routes
 
 # Load the trained model and scaler
 model = joblib.load('random_forest_model.pkl')
 scaler = joblib.load('scaler.pkl')
+
+@app.route('/')
+def index():
+    return send_from_directory('static', 'index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
